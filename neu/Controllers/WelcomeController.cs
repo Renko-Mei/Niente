11ffa476bda87ccb5791cfc97824949b9996801f
xwa132 <xwa132@sfu.ca>
2019-02-17 02:11:41 -0800

    User welcome controller to display dev variable information

--- neu/Controllers/WelcomeController.cs
+++ neu/Controllers/WelcomeController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,11 +13,23 @@ namespace neu.Controllers
     [Route("api/")]
     public class WelcomeController : Controller
     {
+        private readonly IConfiguration Configuration;
+        private readonly IHostingEnvironment HostingEnvironment;
+
+        public WelcomeController(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
+        }
+
         // GET: api/<controller>
         [HttpGet]
         public string Get()
         {
-            return "Welcome to neu API";
+            string welcome = "Welcome to neu API\n" +
+                             $"The environment is {HostingEnvironment.EnvironmentName}\n" +
+                             $"The secret is {Configuration["Bangumi:ClientSecret"]}";
+            return welcome;
         }
     }
 }
