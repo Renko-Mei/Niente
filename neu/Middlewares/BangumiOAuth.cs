2ac6aba970af52db0ed263b1a5865c67d1ba302c
xwa132 <xwa132@sfu.ca>
2019-02-17 22:17:23 -0800

    Add some progress to Bangumi APIs, however this might not be what I want

--- neu/Middlewares/BangumiOAuth.cs
+++ neu/Middlewares/BangumiOAuth.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace neu.Middlewares
@@ -22,12 +28,46 @@ namespace neu.Middlewares
             .AddCookie()
             .AddOAuth("Bangumi", options =>
             {
+                // Add application information
                 options.ClientId = Configuration["Bangumi:ClientId"];
                 options.ClientSecret = Configuration["Bangumi:ClientSecret"];
                 options.CallbackPath = new PathString("/bgmauth");
 
-                options.AuthorizationEndpoint = "https://bgm.tv/oauth/authorize";
+                // Add Bangumi endpoints
+                options.AuthorizationEndpoint = Configuration["Bangumi:AuthEndPoint"];
+                options.TokenEndpoint = Configuration["Bangumi:TokenEndPoint"];
+                options.UserInformationEndpoint = Configuration["Bangumi:UserInfoEndPoint"];
 
+                //options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+                //options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+                //options.ClaimActions.MapJsonKey("urn:github:login", "login");
+                //options.ClaimActions.MapJsonKey("urn:github:url", "html_url");
+                //options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
+                //options.ClaimActions.MapJsonKey();
+
+                options.Events = new OAuthEvents
+                {
+                    OnCreatingTicket = async context =>
+                    {
+                        // Create the request message to get user data via the backchannel
+                        var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+                        // Query user data via the backchannel
+                        var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                        response.EnsureSuccessStatusCode();
+
+                        // Parse user data into an object
+                        var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+                        // Store the token in a cookie
+                        context.HttpContext.Response.Cookies.Append("Bangumi-Token", context.AccessToken);
+
+                        // Execute the defined mapping action by using the received user object
+                        context.RunClaimActions(user);
+                    }
+                };
             });
         }
     }
