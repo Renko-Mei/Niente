1b61d4bf3f01a6f13a466ef2d8994c7088a005b3
xwa132 <xwa132@sfu.ca>
2019-03-06 21:58:31 -0800

    Add article viewmodel

---
+++ Niente/Models/ArticleViewModels/ArticleViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Niente.Models.ArticleViewModels
+{
+    public class ArticleViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+
+        public string PreviewText { get; set; }
+
+        public DateTime CreateAt { get; set; }
+
+        public DateTime LastEditAt { get; set; }
+
+        public string PreviewImageUri { get; set; }
+
+        public string[] ImageUris { get; set; }
+    }
+}
